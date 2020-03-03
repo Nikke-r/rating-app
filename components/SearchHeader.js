a1fdde0eaf73a72e92f1c4d74ee156c96456978b
Niklas Rönnholm <nikke.ronnholm@gmail.com>
2020-03-03 16:24:15 +0200

    search funcionality added to SearchHeader but it lists results just to console atm

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Header, Item, Input, Icon, Button } from 'native-base';
+import { Header, Item, Input, Icon, Button, Text } from 'native-base';
 import { AsyncStorage } from 'react-native';
 
 const SearchHeader = (props) => {
+    const [query, setQuery] = useState();
 
     const handlePress = async () => {
         try {
@@ -13,10 +14,35 @@ const SearchHeader = (props) => {
         }
     };
 
+    useEffect(() => {
+        const search = async () => {
+            try {
+                const token = await AsyncStorage.getItem('token');
+                const options = {
+                    method: 'POST',
+                    headers: {
+                        'x-access-token': token,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({title: query}),
+                };
+                const response = await fetch('http://media.mw.metropolia.fi/wbma/media/search', options);
+                const toJSON = await response.json();
+                console.log('Search: ', toJSON);
+            } catch (error) {
+                console.log('search error: ', error.message);
+            }
+        };
+
+        if (query !== undefined) {
+            search();
+        }
+    }, [query]);
+
     return (
         <Header searchBar rounded>
             <Item>
-                <Input placeholder='Search...' />
+                <Input placeholder='Search...' onChangeText={text => setQuery(text)} />
                 <Button transparent icon>
                     <Icon name='search'/>
                 </Button>
